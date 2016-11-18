5b1cec223384d9db98ef246bbd429d7fcda2127f
wush.wu <wush.wu@generalmobi.com>
2016-11-18 15:52:52 +0800

    generate 100 bid requests for testing

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ var sha1 = require('sha1');
 var randomip = require('random-ip');
 var random_ua = require('random-ua');
 var randgen = require('randgen');
+var pb = require('protocol-buffers');
+var fs = require('fs');
+var nbo = require('network-byte-order');
+var geoip = require('geoip-lite');
 
 var iab = require(__dirname + "/iab.json");
+var schema = pb(fs.readFileSync(__dirname + "/openrtb.proto"));
 
 function iab_generate() {
 	var k = randgen.rpoisson(2) + 1;
@@ -24,57 +29,63 @@ var bidRequestBuilder = openrtb.getBuilder({
 
 var program = require('commander')
 .option("-n, --num [num]", "the size of generated bid requests", parseInt)
+.option("--json_out [json_out]")
+.option("--proto_out [proto_out]")
 .parse(process.argv);
 
-for(var i = 0;i < program.num;i++) {
+var json_out = fs.createWriteStream(program.json_out);
+var proto_out = fs.createWriteStream(program.proto_out);
+
+var app_id = [];
+var app_name = [];
+var publisher_id = [];
+var indexes = [];
+for(var i = 0;i < 5;i++) {
+	app_id.push(uuid());
+	app_name.push(uuid());
+	publisher_id.push(uuid());
+	indexes.push(i);
+}
+function generate_single() {
+	var app_index = randgen.rlist(indexes);
+	var ip = randomip("0.0.0.0", 0);
+	var geo = geoip.lookup(ip);
+	if (geo) {
+		geo = {
+			country : geo.country,
+			region : geo.region
+		};
+		if (geo.ll) {
+			geo.lat = geo.ll[0],
+			geo.lon = geo.ll[1]
+		}
+	} else {
+		geo = {};
+	}
 	var bidRequest = bidRequestBuilder
 	.timestamp(moment.utc().format())
-	.id('1234')
-	.at(2)
-	.imp([
-		{
-			"id":"1",
-			"native":{
-				"request": {
-					"ver": 1,
-					"layout": 6,
-					"assets": [
-						{ "id": 0, "req": 1, "title": { "len": 25 } },
-						{ "id": 1, "req": 1, "img": { "type": 3, "wmin": 100, "hmin": 100 } },
-						{ "id": 3, "req": 0, "data": { "type": 2, "len": 90 } }
-					]
-				}
-			},
-			"tagid": uuid(),
-			"bidfloor": 1.3,
-			"pmp": {
-				"private_auction": 1,
-				"deals": [
-					{
-						"id": 'deal1',
-						"bidfloor": 5.5,
-						"bidfloorcur": "USD",
-						"at": 3,
-						"wseat": ["seat1"],
-						"wadomain": ["advertiser.com"]
-					}
-				]
-			}
+	.id(uuid())
+	.at(randgen.rlist([1,2,3]))
+	.imp([{
+		"id" : uuid(),
+		"banner" : {
+			w : 320,
+			h : 50
 		}
-	])
+	}])
 	.app({
-		"id":uuid(),
-		"name":uuid(),
+		"id":app_id[app_index],
+		"name":app_name[app_index],
 		"cat":iab_generate(),
 		"storeurl": "http://www.example.com",
 		"publisher":{
-				"id": "6332"
+				"id": publisher_id[app_index]
 		}
 	})
 	.device({
 		"dnt":0,
 		"ua":random_ua.generate(),
-		"ip":randomip("0.0.0.0", 0),
+		"ip":ip,
 		"connectiontype":2,
 		"devicetype":1,
 		"didsha1": sha1(uuid()),
@@ -84,9 +95,7 @@ for(var i = 0;i < program.num;i++) {
 		"language": "en",
 		"os": "Android",
 		"osv": "5.1.1",
-		"geo": {
-				"country": "UK"
-		}
+		"geo": geo
 	})
 	.user({
 		"id":uuid(),
@@ -99,5 +108,27 @@ for(var i = 0;i < program.num;i++) {
 		'extra': '1234'
 	})
 	.build();
-	console.log(JSON.stringify(bidRequest));
+	var json = JSON.stringify(bidRequest);
+	json_out.write(json);
+	console.log(json);
+	debugger;
+	var buf = schema.BidRequest.encode(bidRequest);
+	var size_buf = new Buffer(4);
+	nbo.htonl(size_buf, 0, buf.length);
+	proto_out.write(size_buf);
+	proto_out.write(buf);
 }
+
+var num = 0;
+function generate() {
+	if (num < program.num) {
+		generate_single();
+		num += 1;
+		setImmediate(generate);
+	} else {
+		return json_out.end(function() {
+			proto_out.end();
+		});
+	}
+}
+generate();
